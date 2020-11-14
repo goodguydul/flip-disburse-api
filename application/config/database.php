7ef896dbc8f1137c6c21c81fc2a404115719787b
goodguydul <48383804+goodguydul@users.noreply.github.com>
2020-11-14 19:14:17 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -74,8 +74,6 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 // FOR HEROKU APP
-
-
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 if (strpos($actual_link, 'localhost') == false) {
